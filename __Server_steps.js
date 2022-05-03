216a46027dbbbbc1753b93150d29d1f72c390682
mdrashadul-rimon <mdrashadul.rimon@gmail.com>
2022-05-03 20:54:59 +0600

    adding steps

---
+++ __Server_steps.js
@@ -0,0 +1,14 @@
+/**
+ * -------------------------
+ *  One Time
+ * -----------------------
+ * 1. Create heroku account from signup.heroku.com
+ * 2. verify email and setup account
+ * 3. install heroku CLI in computer
+ * 4. heroku login
+ * -------------------------------
+ * for each project
+ * ------------------------------
+ * 1. heroku create
+ * 2. 
+ */
